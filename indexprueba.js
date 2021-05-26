c81190d35da1265b914dee623267ffa66996bfda
lahermanademikel <84536950+lahermanademikel@users.noreply.github.com>
2021-05-26 17:28:44 +0200

    Update indexprueba.js

--- indexprueba.js
+++ indexprueba.js
@@ -9,7 +9,7 @@ const connectToMongoDB = async () =>{
 
             const user ={
                 email: 'testprueba@email.comt',
-                username: 'Maikumike',
+                username: 'megustalahermanadexabi',
                 password: 'Almi123'
             }
             await new userSchema(user).save() 
